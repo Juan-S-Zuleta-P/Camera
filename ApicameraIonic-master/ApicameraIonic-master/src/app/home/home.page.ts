f72feda35f6e80fac87f154396135a5619e85ff5
Juan-S-Zuleta-P <74433609+Juan-S-Zuleta-P@users.noreply.github.com>
2023-10-16 20:07:36 -0500

    Subiendo

---
+++ ApicameraIonic-master/ApicameraIonic-master/src/app/home/home.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class HomePage {
+  
+  imageSource:any;
+
+  constructor() {}
+  takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source:CameraSource.Prompt
+    });
+
+    this.imageSource=image.dataUrl;
+  
+    
+  };
+}
